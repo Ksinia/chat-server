df29ac0e2ed7a2c6cf5c5c79447f9e032b24757f
Ksinia <xiatian@mail.ru>
2019-12-13 11:02:51 +0100

    Add async/await

--- message/router.js
+++ message/router.js
@@ -5,17 +5,22 @@ const { Router } = express; //destructuring
 
 const router = new Router();
 
+// router.get("/message", (req, res, next) => {
+//     Message.findAll()
+//     .then(messages => res.send(messages))
+//     .catch(next); // catch(error=>next(error))
+// });
+
 // этот эндпойнт мы не будем использовать, он только для теста
-router.get("/message", (req, res, next) => {
-  Message.findAll()
-    .then(messages => res.send(messages))
-    .catch(next); // catch(error=>next(error))
+router.get("/message", async (req, res, next) => {
+  const messages = await Message.findAll();
+  res.send(messages);
 });
 
-router.post("/message", (req, res, next) => {
-  Message.create(req.body)
-    .then(message => res.send(message))
-    .catch(next);
+// сейчас модно использовать async await
+router.post("/message", async (req, res, next) => {
+  const message = await Message.create(req.body);
+  res.send(message);
 });
 
 module.exports = router;
