fc3d35ca613a6ee57295f733debcc77b4c2515be
Ksinia <xiatian@mail.ru>
2019-12-13 10:55:07 +0100

    Register post endpoint

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const MessageRouter = require("./message/router");
+const bodyParser = require("body-parser");
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 app.use(MessageRouter);
 
 app.listen(port, () => console.log(`Listening on port : ${port}`));
--- message/router.js
+++ message/router.js
@@ -5,10 +5,17 @@ const { Router } = express; //destructuring
 
 const router = new Router();
 
+// этот эндпойнт мы не будем использовать, он только для теста
 router.get("/message", (req, res, next) => {
   Message.findAll()
     .then(messages => res.send(messages))
     .catch(next); // catch(error=>next(error))
+});
+
+router.post("/message", (req, res, next) => {
+  Message.create(req.body)
+    .then(message => res.send(message))
+    .catch(next);
 });
 
 module.exports = router;
