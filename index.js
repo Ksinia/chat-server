51aae75f5f6117ac17438331ddcf447eebe9ee35
Ksinia <xiatian@mail.ru>
2019-12-13 13:56:57 +0100

    Add streaming

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
-const port = 4000;
-const MessageRouter = require("./message/router");
+const MessageRouterFactory = require("./message/router");
 const bodyParser = require("body-parser");
+const Sse = require("json-sse");
+const Message = require("./message/model");
+
+const port = 4000;
+
+const stream = new Sse();
+const MessageRouter = MessageRouterFactory(stream);
 
 app.get("/", (req, res) => {
+  stream.send("hi");
   res.send("Hello");
+});
+
+//endpoint for connecting to a stream
+app.get("/stream", async (req, res, next) => {
+  try {
+    // get data from DB
+    const messages = await Message.findAll();
+    //serialize (convert array of massages into a string)
+    const string = JSON.stringify(messages);
+    // prepare string for sending to clients as soon as they connect (like loading a gun)
+    stream.updateInit(string);
+    // connect this user to the stream. You don't need to make res.send by yourself. Stream breaks the rule
+    // that you cannot send multiple responses to 1 request.
+    stream.init(req, res);
+  } catch (error) {
+    next(error);
+  }
 });
 
 const jsonParser = bodyParser.json();
--- message/router.js
+++ message/router.js
@@ -3,43 +3,52 @@ const Message = require("./model");
 
 const { Router } = express; //destructuring
 
-const router = new Router();
+// функция, создающая роутер
+function factory(stream) {
+  const router = new Router();
 
-// router.get("/message", (req, res, next) => {
-//     Message.findAll()
-//     .then(messages => res.send(messages))
-//     .catch(next); // catch(error=>next(error))
-// });
+  // router.get("/message", (req, res, next) => {
+  //     Message.findAll()
+  //     .then(messages => res.send(messages))
+  //     .catch(next); // catch(error=>next(error))
+  // });
 
-// этот эндпойнт мы не будем использовать, он только для теста
-router.get("/message", async (req, res, next) => {
-  try {
-    const messages = await Message.findAll();
-    res.send(messages);
-  } catch (error) {
-    next(error);
+  // этот эндпойнт мы не будем использовать, он только для теста
+  router.get("/message", async (req, res, next) => {
+    try {
+      const messages = await Message.findAll();
+      res.send(messages);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  // сейчас модно использовать async await
+  // router.post("/message", async (req, res, next) => {
+  //   try {
+  //     const message = await Message.create(req.body);
+  //     res.send(message);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // });
+
+  //второй варинат написание того же без анонимных функций
+  async function onPost(req, res, next) {
+    try {
+      const message = await Message.create(req.body);
+
+      const string = JSON.stringify(message);
+      stream.send(string);
+
+      res.send(message);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+  router.post("/message", onPost);
 
-// сейчас модно использовать async await
-// router.post("/message", async (req, res, next) => {
-//   try {
-//     const message = await Message.create(req.body);
-//     res.send(message);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-//второй варинат написание того же без анонимных функций
-async function onPost(req, res, next) {
-  try {
-    const message = await Message.create(req.body);
-    res.send(message);
-  } catch (error) {
-    next(error);
-  }
+  return router;
 }
-router.post("/message", onPost);
 
-module.exports = router;
+module.exports = factory;
